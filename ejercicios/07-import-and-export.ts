11c99370eb2e1b6ef6c697e5410437ce36e6a781
medardo123 <86995621+medardo123@users.noreply.github.com>
2022-09-17 10:04:03 -0500

    1 file modified, 1 added

---
+++ ejercicios/07-import-and-export.ts
@@ -0,0 +1,17 @@
+import { calcularIva, Producto } from './06-desestructuracion-de-argumentos';
+
+const carritoCompras: Producto[] = [
+  {
+    desc: 'telefono 1',
+    precio: 34,
+  },
+  {
+    desc: 'telefono 2',
+    precio: 57,
+  },
+];
+
+const [total, iva] = calcularIva(carritoCompras);
+
+console.log('total:', total);
+console.log('iva:', iva);
